0bed969f7ba2d19fa2df7852c83296a178e731e9
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2020-06-09 11:43:57 +0100

    Building a blog -5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from "react"
 import {graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color:blue;
+`
 
 export default ({ data}) => {
   console.log({data})
@@ -16,7 +25,11 @@ export default ({ data}) => {
       {
         data.allMarkdownRemark.edges.map(({node})=>(
           <div key={node.id}>
+            <BlogLink to={node.fields.slug}> 
+            <BlogTitle>
             <h3>{node.frontmatter.title} {node.frontmatter.data}</h3>
+              </BlogTitle>          
+             </BlogLink>
             <p>{node.excerpt}</p>
             <br></br>
           </div>
@@ -31,7 +44,7 @@ export default ({ data}) => {
 
 export const query = graphql`
 query{
-    allMarkdownRemark {
+    allMarkdownRemark(sort:{fields:[frontmatter___date], order: DESC}) {
       totalCount
     edges {
       node {
@@ -40,6 +53,9 @@ query{
           date
           description
           title
+        }
+        fields{
+          slug
         }
         excerpt
       }
